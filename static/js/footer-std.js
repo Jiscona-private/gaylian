546f83d8c7a0e5a1f9cd2da8d5f3d37c3622e11e
SpiderChris <106156319+SpiderChris@users.noreply.github.com>
2022-09-10 23:00:48 +0200

    Index fertig, Footer+ Navbar fertig, agb

--- static/js/footer-std.js
+++ static/js/footer-std.js
@@ -1,6 +1,20 @@
 document.write(`
-<footer class="fw-lighter">
-    © Copyright 2022 by Bonarium Holdings. All Rights Reserved. <br>
-    gaylian.net is powered by Bonarium Holdings.
+<footer>
+    <div class="container text-center">
+        <div class="row align-items-center">
+        <div class="col">
+            <a class="btn btn_footer" href="https://gaylian.net/agb">Geschäftsbedingungen (AGB)</a><br>
+            <a class="btn btn_footer" href="https://gaylian.net/cookies">Cookies</a>
+        </div>
+        <div class="col">
+            © 2022 by Bonarium Holdings. Alle Rechte sind reserviert. <br>
+            gaylian.net ist ein Service von Bonarium Holdings.
+        </div>
+        <div class="col">
+            <a class="btn btn_footer" href="https://gaylian.net/">Auftrag</a><br>
+            <a class="btn btn_footer" href="https://gaylian.net/impressum">Impressum</a>
+        </div>
+        </div>
+    </div>
 </footer>
 `)
