21ccd10708381e472b7e41ec848f679ec7b20ed7
SpiderChris <106156319+SpiderChris@users.noreply.github.com>
2022-09-12 21:20:43 +0200

    kleinkram

--- static/js/footer-std.js
+++ static/js/footer-std.js
@@ -6,7 +6,7 @@ document.write(`
             <a class="btn btn_footer" href="https://gaylian.net/agb">Geschäftsbedingungen (AGB)</a><br>
             <a class="btn btn_footer" href="https://gaylian.net/cookies">Cookies</a>
         </div>
-        <div class="col">
+        <div class="col text_footer">
             © 2022 by Bonarium Holdings. Alle Rechte sind reserviert. <br>
             gaylian.net ist ein Service von Bonarium Holdings.
         </div>
