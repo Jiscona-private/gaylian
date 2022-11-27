9f73d2084be8da7558845664cb5be86190c44e89
SpiderChris <106156319+SpiderChris@users.noreply.github.com>
2022-11-27 14:26:16 +0100

    Introducing mail html stuff
    created a nav-mail.js + mail.html

---
+++ static/js/nav-mail.js
@@ -0,0 +1,87 @@
+//user management element
+user = ""
+
+if (document.cookie) {
+  username = document.cookie.split(";")[1].split("=")[1]
+  user=`
+  <li class="nav-item dropdown">
+    <a class="nav-coming btn btn_nav dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+      Hi ${username}
+    </a>
+    <ul class="dropdown-menu" style="padding-right: 5px;">
+      <li><a class="dropdown-item" href="https://gaylian.net/user/data">Meine Dateien</a></li>
+      <li><hr class="dropdown-divider"></li>
+      <li><a class="dropdown-item" href="/user/logout?username=${username}" data-bs-target="logout">Logout</a></li>
+    </ul>
+  </li>`
+} 
+
+document.write(`
+    <div class="nav_all">
+      <nav class="navbar sticky-top navbar-expand-lg navbar-scroll">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="https://gaylian.net">
+              <img src="../static/img/gaylian_50.png" alt="Logo" width="40" height="40">
+            </a>
+            <a class="navbar-brand fs-3 nav-shadow" href="https://gaylian.net" style="padding-bottom: 10px;">gaylian.net</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-mail btn btn_nav" href="https://gaylian.net/mail/home">Mail</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-mail btn btn_nav" href="https://gaylian.net/mail/inbox">Mail-Inbox</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-mail btn btn_nav" href="https://gaylian.net/mail/spam">Mail-Spam</a>
+                </li>
+              </ul>
+              <ul class="navbar-nav" style="padding-right: 5px;">
+                <li class="nav-item" style="padding-right: 5px;">
+                  <a class="nav-coming btn btn_nav" href="https://gaylian.net/login">Login</a>
+                </li>
+                ${user}
+              </ul>
+              <div style="padding-bottom: 1px;">
+                <button class="navbar-toggler_index navbar-text" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              </div>
+            </div>
+        </div>
+      </nav>
+      <div class="collapse collapse_content" id="navbarToggleExternalContent">
+        <div class="container text-center div_nav_cola">
+          <div class="row align-items-center">
+            <div class="p-3">
+              <h5 class="h4">gaylian.net | Weitere Links</h5>
+              <span class="text-muted">Ihre erweiterte Navigation</span>
+            </div>
+          </div>
+        </div>
+        <div class="container text-center div_nav_cola">
+          <div class="row align-items-center">
+            <div class="col">
+              <a class="btn btn_footer" href="https://gaylian.net/agb">Geschäftsbedingungen (AGB)</a><br>
+              <a class="btn btn_footer btn_footer_p" href="https://gaylian.net/cookies">Cookies</a>
+            </div>
+            <div class="col">
+              © 2022 by Bonarium Holdings. Alle Rechte sind reserviert. <br>
+              gaylian.net ist ein Service von Bonarium Holdings.
+            </div>
+            <div class="col">
+              <a class="btn btn_footer" href="https://gaylian.net/">Auftrag</a><br>
+              <a class="btn btn_footer btn_footer_p" href="https://gaylian.net/impressum">Impressum</a>
+            </div>
+            <hr style="margin-top: 10px;">
+            <div class="p-2">
+              basiert. basierter. gaylian.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  `)
